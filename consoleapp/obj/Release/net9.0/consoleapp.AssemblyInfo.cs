47346fdd6c2692952c735220aee13a8ea1d34e6b
Alexis Vera S. <alxvcs93@hotmail.com>
2025-07-01 20:03:36 -0500

    Análisis de Archivos de Compilación en .NET

--- consoleapp/obj/Release/net9.0/consoleapp.AssemblyInfo.cs
+++ consoleapp/obj/Release/net9.0/consoleapp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("consoleapp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631e692d5ddb5cd88f458e4f0a27d909b25e0766")]
 [assembly: System.Reflection.AssemblyProductAttribute("consoleapp")]
