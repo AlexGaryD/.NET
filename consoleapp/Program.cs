691d385f0dc4acf60e7a45db64300e648f026063
Alexis Vera S. <alxvcs93@hotmail.com>
2025-07-01 21:19:10 -0500

    Implementación de Librería Humanizer en .NET para Números en Palabras

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,9 +1,10 @@
-﻿Console.WriteLine("Ingresa un nombre!"); // Solicita al usuario que ingrese un nombre
+﻿using Humanizer; // Importa la biblioteca Humanizer para formatear cadenas de texto
+Console.WriteLine("Ingresa un nombre!"); // Solicita al usuario que ingrese un nombre
 var nombre = Console.ReadLine(); // Lee la entrada del usuario
 Console.WriteLine("Por favor, ingrese su cargo"); // Solicita al usuario que ingrese su cargo
 var cargo = Console.ReadLine(); // Lee la entrada del usuario
 Console.WriteLine("Por favor, ingrese su edad"); // Solicita al usuario que ingrese su edad
-var edad = Console.ReadLine(); // Lee la entrada del usuario
+var edad = int.Parse(Console.ReadLine()); // Lee la entrada del usuario
 
-Console.WriteLine($"Hola, Mi nombre es {nombre}, mi cargo es{cargo}, y tengo {edad} años"); // Imprime la información ingresada por el usuario
+Console.WriteLine($"Hola, Mi nombre es {nombre}, mi cargo es{cargo}, y tengo {edad.ToWords()} años"); // Imprime la información ingresada por el usuario
 Console.WriteLine("Presiona cualquier tecla para salir..."); // Solicita al usuario que presione una tecla para salir
--- consoleapp/obj/Debug/net9.0/consoleapp.AssemblyInfo.cs
+++ consoleapp/obj/Debug/net9.0/consoleapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47346fdd6c2692952c735220aee13a8ea1d34e6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58b5883e0bb4b7bc01cea46d1f6b7c575f9ead9")]
 [assembly: System.Reflection.AssemblyProductAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("consoleapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
